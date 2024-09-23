13329f13846af89d849f75c56573b14cc6ecd312
SzBenede2006 <bndkszabo@gmail.com>
2024-09-23 17:50:29 +0200

    Just the basics

--- app/src/main/java/dev/benedek/rig/MainActivity.kt
+++ app/src/main/java/dev/benedek/rig/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.benedek.rig.ui.theme.RIGTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
             RIGTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     RigUi(
-                        name = "Android",
+                        name = "RIG",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,13 +43,28 @@ class MainActivity : ComponentActivity() {
 fun RigUi(name: String, modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
-
+            .fillMaxSize(),
     ){
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+        Column (
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            Text(
+                modifier = modifier
+                    .padding(top = 32.dp),
+                text = "Say hello to $name!",
+                //lineHeight = 8.sp,
+                fontSize = 36.sp
+            )
+            Text(
+                text = "Nice to see you here!",
+                modifier = modifier,
+                //lineHeight = 8.sp,
+                fontSize = 16.sp,
+            )
+        }
+        
     }
 
 }
@@ -49,7 +72,12 @@ fun RigUi(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true, showSystemUi = true, name = "Main screen")
 @Composable
 fun GreetingPreview() {
-    RIGTheme {
-        RigUi("Android")
-    }
+        RIGTheme {
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                RigUi(
+                    name = "RIG",
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
+        }
 }
