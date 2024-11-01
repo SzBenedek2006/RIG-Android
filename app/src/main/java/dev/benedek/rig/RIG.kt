e8bccfd918a40fc760712e083ee330b61c668e9a
Benedek Szabó <bndkszabo@gmail.com>
2024-11-01 09:32:46 +0100

    Fixed JPEG generation and removed unnecessary thread in png

--- app/src/main/java/dev/benedek/rig/RIG.kt
+++ app/src/main/java/dev/benedek/rig/RIG.kt
@@ -2,12 +2,9 @@ package dev.benedek.rig
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.ImageFormat
 import android.util.Log
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.snapshots.Snapshot
 import java.io.File
 import java.io.FileOutputStream
 
@@ -49,41 +46,28 @@ class RIG() {
         val format = "png"
         var file = Array<File>(count){ File("${outputPath}/image${it}.$format") }
 
-        val pngThread = Thread {
-            for (num in 0 until count) {
+        for (num in 0 until count) {
 
-                if (stop.value) {
-                    Log.d("RIG", "Png thread interrupted")
-                    return@Thread
-                }
-                currentCount.intValue = num + 1
+            if (stop.value) {
+                Log.d("RIG", "PNG gen interrupted")
+                return file
+            }
 
-                val bitmap = if (alpha){
-                    genBitmapAlpha(width, height, progressPercent)
-                } else {
-                    genBitmap(width, height, progressPercent)
-                }
+            currentCount.intValue = num + 1
 
+            val bitmap = if (alpha){
+                genBitmapAlpha(width, height, progressPercent)
+            } else {
+                genBitmap(width, height, progressPercent)
+            }
 
+            file[num] = File("${outputPath}/image${num+1}.$format")
 
-                // Saving bitmap as a PNG
-                file[num] = File("${outputPath}/image${num+1}.$format")
-
-
-                // Create a file output stream to save the bitmap
-                FileOutputStream(file[num]).use { outputStream ->
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream) // Save as PNG
-                }
+            FileOutputStream(file[num]).use { outputStream ->
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream) // Save as PNG
             }
         }
-        pngThread.start()
 
-
-
-
-
-
-        pngThread.join()
         return file
 
     }
@@ -102,20 +86,20 @@ class RIG() {
         var file = Array<File>(count){ File("${outputPath}/image${it}.$format") }
 
 
-        // for (int i = 0; i <= 10; i++)
+
         for (num in 0 until count) {
 
+            if (stop.value) {
+                Log.d("RIG", "JPEG gen interrupted")
+                return file
+            }
+            
             currentCount.intValue = num + 1
 
             val bitmap = genBitmap(width, height, progressPercent)
 
-
-
-            // Saving bitmap as a PNG
             file[num] = File("${outputPath}/image${num+1}.$format")
 
-
-            // Create a file output stream to save the bitmap
             FileOutputStream(file[num]).use { outputStream ->
                 bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream) // Save as PNG
             }
